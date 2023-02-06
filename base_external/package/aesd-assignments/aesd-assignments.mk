ad56a684ba6ab7b6f3906391d7f19305889fb5b8
John Jared Creech <john.creech@colorado.edu>
2023-02-06 16:06:11 -0700

    assignment-4-part-2

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = bfb79d327689474cd206d9b6ce676a7c1ccb3d92
+AESD_ASSIGNMENTS_VERSION = 492e40f767bdc0a0641c6a16e3242e1cb2272507
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
