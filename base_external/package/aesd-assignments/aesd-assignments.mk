134fca0d30041d4a61dd5a3d2a84b0f661f799a4
John Jared Creech <john.creech@colorado.edu>
2023-04-04 15:38:06 -0600

    assignment-7-part-2

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 6e1b1486b20749bb2d0090b7034dd20089dcd0fb
+AESD_ASSIGNMENTS_VERSION = f1d22708679678411756308da1edc72497abab6c
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -13,5 +13,8 @@ LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-jaredcreech.git
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 
+LDD_MODULE_SUBDIRS = misc-modules scull
+LDD_MODULE_MAKE_OPTS = KERNELRELEASE=$(LINUX_VERSION_PROBED)
+
 $(eval $(kernel-module))
 $(eval $(generic-package))
